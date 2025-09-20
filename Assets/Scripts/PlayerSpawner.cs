1c09794dd4fc1c6c090e523efa2da36d6d065de9
ARAV1NDPC\Aravindan <aravindanputhalath@gmail.com>
2025-09-20 16:56:32 +0530

    Player spawning.

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,19 @@
+using Fusion;
+using UnityEngine;
+
+public class PlayerSpawner : SimulationBehaviour, IPlayerJoined, IPlayerLeft
+{
+    [SerializeField] private GameObject PlayerPrefab;
+    public void PlayerJoined(PlayerRef player)
+    {
+        if (player == Runner.LocalPlayer)
+        {
+            Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+
+    public void PlayerLeft(PlayerRef player)
+    {
+
+    }
+}
